dbde0fd0c9db1c0c803863c0aa92351743e043b3
drprijkers <drprijkers@me.com>
2017-07-26 22:15:41 +0200

    Version 2 (menu and hover picture_me (with bug))

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+
 
 $(document).ready(function () {
-    'use strict';
+    "use strict";
 
-    $('#letter').fadeIn('slow');
+$(window).load(function() {
+   $(".btn-nav").on("click tap", function() {
+     $(".nav-container").toggleClass("showNav hideNav").removeClass("hidden");
+     $(this).toggleClass("animated");
+   });
+ });
 
 
-
+$("#hover_gone").hover(function()   {
+    $(this).hide();
+},
+    function()  {
+    $(this).show("span");
+});
 
 });
+
+/*$("#hover_gone").mouseout(function()   {
+    $(this).show("span");
+    });*/
+
