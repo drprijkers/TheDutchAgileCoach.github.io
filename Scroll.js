15522ae31f6ab2aaa117021915ea2b72b8ec500f
drprijkers <drprijkers@me.com>
2017-09-03 21:56:32 +0200

    Add files via upload

---
+++ Scroll.js
@@ -0,0 +1,9 @@
+$(document).ready(function (){
+        $('#scroll li').click(function() {
+            var target = $(this).attr('id');
+            $('html, body').animate({scrollTop: $('#'+target+'_content').offset().top - 245}, 1000);
+
+        });
+});
+
+/* top - xxx vervangen door waarde die height wegneemt van menu bar e.d. om exact te scrollen?*/
